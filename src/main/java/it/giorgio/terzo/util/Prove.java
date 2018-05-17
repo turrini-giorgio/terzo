ba8e11dd531b41d041286132caba9fd858b6b9cb
Giorgio Turrini <gturrini@sixtema-ict.it>
2018-05-17 11:13:44 +0200

    Piccole modifiche dopo lombok

--- src/main/java/it/giorgio/terzo/util/Prove.java
+++ src/main/java/it/giorgio/terzo/util/Prove.java
@@ -2,8 +2,6 @@ package it.giorgio.terzo.util;
 
 import it.giorgio.terzo.entity.Utente;
 
-//import it.giorgio.terzo.entity.Utente;
-
 public class Prove {
 	public static void P01() {
 		Utente u = new Utente();
